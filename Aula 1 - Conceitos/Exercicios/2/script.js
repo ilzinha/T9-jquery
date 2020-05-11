021d7c3999bdd97ff83919d2820dff96fb1a2875
Ilza <ilzammed2@gmail.com>
2020-05-11 16:08:59 -0300

    resoluçao dos exercicios 1 e 2

--- Aula 1 - Conceitos/Exercicios/2/script.js
+++ Aula 1 - Conceitos/Exercicios/2/script.js
@@ -5,3 +5,34 @@
   - Você pode criar uma função "changeBg" que será responsável pela lógica dessa alteração
   - Extra: Modifiquem o estilo da página para ela ficar com a sua cara!
 */
+
+
+$(document).ready(function () {
+
+  let bodyColor = $('body')
+  bodyColor.css('background', 'white')
+
+
+  let changeBg = () => {
+
+    if (bodyColor[0].style.background == 'white') {
+      $('body').css('background', '#666')
+      $('h1')
+        .text('Desligado')
+        .css('color', 'white')
+      $('.imagem').attr('src', 'https://media.giphy.com/media/HL1MeEq6XebEA/giphy.gif')
+
+    } else {
+      $('body').css('background', 'white')
+      $('h1')
+        .text('Ligado')
+        .css('color', '#000')
+      $('.imagem').attr('src', 'https://media.giphy.com/media/3KVRMtLXfORVosk6TW/giphy.gif')
+
+    }
+  }
+
+  $('#botao').click(changeBg)
+
+
+})

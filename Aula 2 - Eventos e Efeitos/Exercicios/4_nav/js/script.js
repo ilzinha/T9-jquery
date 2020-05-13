387a9a3102c98b86c642e4f3db3729d0dcfe9463
Ilza <ilzammed2@gmail.com>
2020-05-13 14:38:44 -0300

    comentarios no exercicio 3 e resoluçao do exercicio 4

--- Aula 2 - Eventos e Efeitos/Exercicios/4_nav/js/script.js
+++ Aula 2 - Eventos e Efeitos/Exercicios/4_nav/js/script.js
@@ -1,18 +1,32 @@
 //1. Faça com que os elementos dentro do menu aparecem quando o mouse passar por eles.
 //2. Os elementos devem desaparecer quando o mouse não estiver passando por eles
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $('ul').each(function() {
 
-        $('.menu1').mouseover(function() {
-            $('.menu2').show('slow')
-        })
-    
-        $('.menu1').mouseout(function(){
-            $('.menu2').hide('slow')
-        })
+    //Dessa forma os tres aparecem de vez
+    // $('.menu1').mouseover(function() {
+    //     $('.menu2').show('slow')
+    // })
+
+    // $('.menu1').mouseout(function(){
+    //     $('.menu2').hide('slow')
+    // })
+
+    //ou
+
+    // $('li').hover(function() {
+    //         $(this).find('.menu2').slideToggle()
+    // })
+
+
+    //ou
+
+    $('li').hover(function () {
+        $(this).find('.menu2').slideDown()
+    }, 
+    function() {
+        $(this).find('.menu2').slideUp()
     })
-
 
 })
